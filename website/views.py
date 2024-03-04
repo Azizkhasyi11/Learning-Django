e3b2e350a0e0402fd8e3ab07daaaf32cd28cdbaf
Aziz Khasyi <azizkhasyi11@gmail.com>
2024-03-04 20:47:21 +0700

    Add requirement.txt and add home in navbar

--- website/views.py
+++ website/views.py
@@ -39,7 +39,7 @@ def register_user(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, 'You have been registered and logged in!')
+            messages.success(request, "You have been registered and logged in!")
             return redirect('home')
     else:
         form = SignUpForm()
